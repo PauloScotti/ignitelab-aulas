ef84900158b8ac96a7ae1deaf02aab7ce91d6fd7
PauloScotti <paulo.scotti@hotmail.com>
2022-10-12 20:31:30 -0300

    aulas do ignite lab

---
+++ storybook-static/assets/preview.264b849b.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.70b9e2fc.js";import"./iframe.feea353b.js";import"./es.map.constructor.bdaf42db.js";import"./es.number.to-fixed.ff0d88e4.js";import"./index.592b611e.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.264b849b.js.map
